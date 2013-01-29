981ddf03aa6730f5142830a3c60a04d74a3d4771
Faryshta <angeldelcaos@gmail.com>
2013-01-29 05:37:41 -0600

    syntax changes according to PSR-2

--- library/Rain/DB/StatementIterator.php
+++ library/Rain/DB/StatementIterator.php
@@ -11,14 +11,16 @@ class StatementIterator implements \Iterator
     protected $statement, $fetch_mode, $key = 0, $value;
     protected $select_key, $select_value;
 
-    public function __construct(\PDOStatement $statement, $fetch_mode, $key = null, $value = null){
+    public function __construct(\PDOStatement $statement, $fetch_mode, $key = null, $value = null)
+    {
         $this->statement    = $statement;
         $this->select_key   = $key;
         $this->select_value = $value;
         $this->fetch_mode   = $fetch_mode;
     }
 
-    public function rewind(){
+    public function rewind()
+    {
         if ($this->statement->execute()) {
             $this->iterate();
         } else {
@@ -26,42 +28,46 @@ class StatementIterator implements \Iterator
         }
     }
 
-    public function valid(){
+    public function valid()
+    {
         return (bool) $this->value;
     }
 
-    public function current(){
+    public function current()
+    {
         return $this->value;
     }
 
-    public function next(){
+    public function next()
+    {
         $this->iterate();
     }
     
-    public function key(){
+    public function key()
+    {
         return $this->key;
     }
     
-    protected function iterate(){
+    protected function iterate()
+    {
 
         // The value of the iterator is a selected field of the query
-        if( $this->select_value ){
+        if ($this->select_value) {
             // save the result field in $this->value
             $field = $this->statement->fetchColumn(0);
             $this->value = $field;
                 
-        }
-        else{
+        } else {
             // save the result row in $this->value
             $row = $this->statement->fetch( $this->fetch_mode );
             $this->value = $row;
         }
         
-        if( $this->select_key )
+        if ($this->select_key) {
             $this->key = $row[ $this->select_key ];
-        else
+        } else {
             $this->key++;
-
+        }
         
     }
 
